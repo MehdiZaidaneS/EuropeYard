466b96600b2e42c3aab36a7a5dfc275c54338c93
mehdi <mehdicontacto@gmail.com>
2024-10-18 22:16:47 +0300

    Instructions added

--- start.py
+++ start.py
@@ -1,32 +1,32 @@
 from player import *
 from misterx import *
+from instructions import *
 
 
 
 
 def start():
 
-    print("Welcome to European Yard!")
-    mxspawnpoint()
-    spawnpoint()
-    while turns > 0:
-        xrandommove()
-        moves()
-        if mxpositionreveal() == positionReveal():
-            print("You caught Mx")
-            break
+    print("Welcome to European Yard! Choose one to continue:\n"
+          "['Play', 'Instructions', 'Exit']")
 
+    option = input()
+    if option == 'Play':
+        mxspawnpoint()
+        spawnpoint()
+        while turns > 0:
+            xrandommove()
+            moves()
+            if mxpositionreveal() == positionReveal():
+                print("Congratulations, you caught Mister X")
+                break
 
+    elif option == 'Instructions':
+        aboutgame()
+        rules()
 
-
-
-
-
-
-
-
-
-
+    elif option == 'Exit':
+        print("Thank you for playing!")
 
 
 
